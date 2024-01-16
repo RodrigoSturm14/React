9f3f8d5abbe32e33a6a146f0c6bde4b3b8e361f1
Rodrigo Sturm <rodristurm@gmail.com>
2024-01-16 10:15:19 -0300

    Movi constantes a carpeta de constantes

---
+++ projects/Tic-Tac-Toe/src/constants.js
@@ -0,0 +1,18 @@
+export const TURNS = {
+   X: 'x',
+   O: 'o'
+};
+
+export const WINNER_COMBOS = [
+   // jugadas horizontales
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   // jugadas verticales
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   // jugadas diagonales
+   [0, 4, 8],
+   [2, 4, 6]
+];
