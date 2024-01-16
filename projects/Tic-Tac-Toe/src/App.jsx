d5398906a22a28168d9f2b39232fe0c794c257b4
Rodrigo Sturm <rodristurm@gmail.com>
2024-01-16 10:17:40 -0300

    Componetice el codigo

--- projects/Tic-Tac-Toe/src/App.jsx
+++ projects/Tic-Tac-Toe/src/App.jsx
@@ -1,37 +1,13 @@
 import { useState } from 'react';
 import confetti from 'canvas-confetti';
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const Square = ({children, updateBoard, isSelected, index}) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`;
-  const handleClick = () =>{
-    updateBoard(index);
-  };
-
-  return(
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  // jugadas horizontales
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  // jugadas verticales
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  // jugadas diagonales
-  [0, 4, 8],
-  [2, 4, 6]
-]
+// componentes
+import { Square } from './components/Square.jsx';
+import { Board } from './components/Board.jsx';
+import { WinnerModal } from './components/WinnerModal.jsx';
+// constantes
+import { TURNS } from './constants.js';
+// funciones
+import { checkWinner, checkEndGame } from './logic/board.js';
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
@@ -42,30 +18,10 @@ function App() {
   // useState para saber si hay ganador y quien es el ganador
   const [winner, setWinner] = useState(null); // null es q no hay ganador todavia, se sigue jugando; false es q hay empate, se completaron todos los cuadrados
 
-  const checkWinner = (boardToCheck) =>{
-    for(const combo of WINNER_COMBOS){
-      const [a, b, c] = combo; // a, b y c son indices, q van a servir para evaluar las posiciones de boardToCheck, o sea 'newBoard'
-      if(
-        boardToCheck[a] && // si en la posicion a tiene algo diferente a null
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ){
-        // devolver ganador
-        return boardToCheck[a];
-      }
-    }
-    // devolver si no hay ganador todavia
-    return null;
-  }
-  
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
-  }
-
-  const checkEndGame = (newBoard) =>{
-    return newBoard.every((square) => square !== null);
   }
 
   // actualizar tablero
@@ -100,19 +56,7 @@ function App() {
       <h1>Hello Tic Tac Toe!</h1>
       <button onClick={resetGame}>Reset Game</button>
       <section className="game">
-        {
-          board.map((_, index) => {
-            return(
-              <Square 
-                key={index}
-                index={index}
-                updateBoard={updateBoard}
-              >
-                {board[index]}
-              </Square>
-            );
-          })
-        }
+        <Board board={board} updateBoard={updateBoard} />
       </section>
       <section className='turn'>
         <Square isSelected={turn === TURNS.X} >
@@ -123,28 +67,7 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
-      {
-        winner !== null && (
-          <section className='winner'>
-            <div className='text'>
-              <h2>
-                {
-                  winner === false ? 'Empate' : 'Gano: '
-                }
-              </h2>
-              <header className='win'>
-                {winner && <Square>{winner}</Square>}
-              </header>
-
-              <footer>
-                <button onClick={resetGame}>
-                  Empezar de nuevo
-                </button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
+      <WinnerModal winner={winner} resetGame={resetGame}/>
     </main>
   )
 }
