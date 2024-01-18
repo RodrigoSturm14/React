58c825afd9dc73bc2e9ca618469991f46c43b852
Rodrigo Sturm <rodristurm@gmail.com>
2024-01-18 16:42:08 -0300

    cree componente producto - filtrar productos funcional

--- projects/Shopping-Cart/src/components/Product.jsx
+++ projects/Shopping-Cart/src/components/Product.jsx
@@ -6,11 +6,12 @@ export function Product({ products }){
     <main className='products'>
       <ul>
         {
-          products.map(product => (
+          products.slice(0, 10).map(product => (
               <li key={product.id}>
                 <img src={product.thumbnail} alt={product.title} />
                 <div>
                   <strong>{product.title}</strong>
+                  <span> - ${product.price}</span>
                 </div>
                 <div>
                   <button>
