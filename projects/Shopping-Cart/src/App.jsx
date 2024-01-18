af07340b8e3380a0829ffe2507041b8d431d7780
Rodrigo Sturm <rodristurm@gmail.com>
2024-01-18 16:39:03 -0300

    agregue componente header - useState para los filtros - logica filtros

--- projects/Shopping-Cart/src/App.jsx
+++ projects/Shopping-Cart/src/App.jsx
@@ -1,15 +1,36 @@
-import { products } from "./mocks/products.json";
+import { products as initialProducts } from "./mocks/products.json";
+// componentes
 import { Product } from "./components/Product.jsx";
+import { Header } from "./components/Header.jsx";
+import { useState } from "react";
 
 function App() {
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  });
+
+  const filterProducts = (products) =>{
+    return products.filter(product => {
+      return(
+        product.price >= filters.minPrice && (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      );
+    });
+  };
+
+  const filteredProducts = filterProducts(products);
+
   return (
     <>
-      <h6>
-        hola cart
-      </h6>
-      <Product products={products}/>
+      <Header changeFilters={setFilters}/>
+      <Product products={filteredProducts}/>
     </>
-  )
-}
+    
+  );
+};
 
 export default App
