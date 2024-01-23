d7f71b0abbb680906a55858d200670e351677741
Rodrigo Sturm <rodristurm@gmail.com>
2024-01-23 10:07:21 -0300

    Organice estructura en componentes header - productcard

--- projects/Product-Card/src/App.jsx
+++ projects/Product-Card/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
-import { ProductCard } from "./ProductCard";
+import { ProductCard } from "./components/ProductCard.jsx";
+import { Header } from './components/Header.jsx';
+import { productsJson as initialProductsJson } from './mocks/productsJson.json'
+// header - products - footer
 
 export function App(){
    return(
-      <section className="App">
-         <ProductCard initialCartState={false} prName='Ceramico piso color Cafe A 55x55' prStock='15' imgURL='https://distribuidoramariscal.com.gt/wp-content/uploads/2021/03/piso-ceramico-mar-de-plata-cafe-comp-705x705.jpg'/>
-         <ProductCard initialCartState={false} prName='Rulemanes Timken Hm518445/10inch' prStock='10' imgURL='https://th.bing.com/th/id/OIP.VTrBsz-KdnJwLudKoPu_fQAAAA?rs=1&pid=ImgDetMain'/>
-         <ProductCard initialCartState={false} prName='Bulones cabeza hexagonal de hierro Metrica MA' prStock='45' imgURL='https://th.bing.com/th/id/R.45d9d1c1fb6424d6a98822d98122acfe?rik=7Gh%2b2S5V8gNPXA&riu=http%3a%2f%2fcasoni.com.ar%2fwp-content%2fuploads%2f2018%2f05%2fmaferbul.bulon_.cabeza.hexagonal.G2.jpg&ehk=eTD3%2bAdazRb7pQ2sIF%2f0SpXPuQac0RYeJh236Djv17k%3d&risl=&pid=ImgRaw&r=0'/>
-      </section>
+      <>
+         <Header />
+         <ProductCard initialCartState={false} productsJson={initialProductsJson} />
+      </>
    );
 };
