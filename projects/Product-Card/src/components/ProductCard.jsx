910406e26443a5ffe18bf8cbd13e0a4f15756f26
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-02 17:38:25 -0300

    Cambie nombre de prop producto

--- projects/Product-Card/src/components/ProductCard.jsx
+++ projects/Product-Card/src/components/ProductCard.jsx
@@ -2,54 +2,54 @@ import { useState } from 'react';
 import { CartFilled, CartNotFilled } from './Icons.jsx';
 import './ProductCard.css'
 
-export function ProductCard({ productsJson , initialCartState}){
-   const [cartState, setCart] = useState(initialCartState);
+export function ProductCard({ filteredProducts , initialCartState}){
+  const [cartState, setCart] = useState(initialCartState);
 
-   const cartNotFilled = <CartNotFilled />
-   const cartFilled = <CartFilled />
+  const cartNotFilled = <CartNotFilled />
+  const cartFilled = <CartFilled />
 
-   const cart = cartState ? cartFilled : cartNotFilled ;
-   const bttCartClassName = cartState ? 'prCard-cartButton cartFilled' : 'prCard-cartButton cartNotFilled';
+  const cart = cartState ? cartFilled : cartNotFilled ;
+  const bttCartClassName = cartState ? 'prCard-cartButton cartFilled' : 'prCard-cartButton cartNotFilled';
 
-   const handleClick = () =>{
-      setCart(!cartState);
-   }
-   
-   return(
-      <>
-         <section className='productsWrap'>
-            <ul>
-               {
-                  productsJson.map(productsJson => (
-                     <li key={productsJson.id} className="prCard">
-                        <div className="prCard-imageContainer">
-                           <img className="prCard-image" alt="Repositor Automatico - Inicio" src={productsJson.image} />
-                        </div>
+  const handleClick = () =>{
+    setCart(!cartState);
+  }
+  
+  return(
+    <>
+      <section className='productsWrap'>
+        <ul>
+          {
+            filteredProducts.map(filteredProducts => (
+              <li key={filteredProducts.id} className="prCard">
+                <div className="prCard-imageContainer">
+                  <img className="prCard-image" alt="Repositor Automatico - Inicio" src={filteredProducts.image} />
+                </div>
 
-                        <div className="prCard-infoContainer">
-                           <h3 className="prCard-prName">
-                              {productsJson.name}
-                           </h3>
-                           <h4 className="prCard-prStock">
-                              Cajas disponibles: {productsJson.stock}
-                           </h4>
-                        </div>
-                        <div className="prCard-buttonContainer">
-                           <button className="prCard-orderButton">
-                              Ordenar
-                           </button>
-                           <button className={bttCartClassName} onClick={handleClick}>
-                              <span className='prCard-cartButton-FillIt'>{cart}</span>
-                              <span className='prCard-cartButton-dontFillIt'>
-                                 <CartFilled />
-                              </span>
-                           </button>
-                        </div>
-                     </li>
-                  ))
-               }
-            </ul>
-         </section>
-      </>
-   );
+                <div className="prCard-infoContainer">
+                  <h3 className="prCard-prName">
+                    {filteredProducts.name}
+                  </h3>
+                  <h4 className="prCard-prStock">
+                    Cajas disponibles: {filteredProducts.stock}
+                  </h4>
+                </div>
+                <div className="prCard-buttonContainer">
+                  <button className="prCard-orderButton">
+                    Ordenar
+                  </button>
+                  <button className={bttCartClassName} onClick={handleClick}>
+                    <span className='prCard-cartButton-FillIt'>{cart}</span>
+                    <span className='prCard-cartButton-dontFillIt'>
+                      <CartFilled />
+                    </span>
+                  </button>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      </section>
+    </>
+  );
 };
