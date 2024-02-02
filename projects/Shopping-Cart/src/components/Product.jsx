32423464aeb926117d9eadd237df9afdf57f6ff6
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-02 11:21:38 -0300

    Uso de context del cart, checkeo si producto esta en el carrito y actualizacion de boton 'agregar al carrito'

--- projects/Shopping-Cart/src/components/Product.jsx
+++ projects/Shopping-Cart/src/components/Product.jsx
@@ -1,12 +1,21 @@
 import './Products.css';
-import { AddToCartIcon } from './Icons.jsx';
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons.jsx';
+import { useCart } from '../hooks/useCart.js';
 
 export function Product({ products }){
+  const {addToCart, removeFromCart, cart} = useCart()
+
+  const checkProductInCart = (product) => {
+    return cart.some(item => item.id === product.id)
+  }
+
   return(
     <main className='products'>
       <ul>
         {
-          products.slice(0, 10).map(product => (
+          products.slice(0, 10).map(product => {
+            const isProductInCart = checkProductInCart(product)
+            return(
               <li key={product.id}>
                 <img src={product.thumbnail} alt={product.title} />
                 <div>
@@ -14,13 +23,18 @@ export function Product({ products }){
                   <span> - ${product.price}</span>
                 </div>
                 <div>
-                  <button>
-                    <AddToCartIcon />
+                  <button style={{backgroundColor: isProductInCart ? 'red' : '#09f'}} onClick={() => isProductInCart ? removeFromCart(product) : addToCart(product)}>
+                    {
+                      isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />
+                    }
+                    {
+                      console.log(isProductInCart)
+                    }
                   </button>
                 </div>
               </li>
             )
-          )
+          })
         }
       </ul>
     </main>
