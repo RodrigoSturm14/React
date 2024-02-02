46c0a44a345f067e2e17bf4a98a1043db44dd997
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-02 11:11:12 -0300

    Agregue componente cart a la derecha y envolvi en app con contexto CartProvider

--- projects/Shopping-Cart/src/App.jsx
+++ projects/Shopping-Cart/src/App.jsx
@@ -1,11 +1,15 @@
 import { products as initialProducts } from "./mocks/products.json";
 import { IS_DEVELOPMENT } from "./config.js";
+// contextos
+import { CartProvider } from "./context/cart.jsx";
 // componentes
 import { Product } from "./components/Product.jsx";
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
+import { Cart } from "./components/Cart.jsx";
 // hooks
 import { useFilters } from "./hooks/useFilters.js";
+import './components/Cart.css'
 
 function App() {
   // custom hook q devuelve funcion 'filterProducts' y una forma de actualizar el estado de 'filters', 'setFilters'
@@ -14,11 +18,12 @@ function App() {
   const filteredProducts = filterProducts(initialProducts);
 
   return (
-    <>
+    <CartProvider>
       <Header />
+      <Cart />
       <Product products={filteredProducts}/>
       {IS_DEVELOPMENT && <Footer />}
-    </>
+    </CartProvider>
   );
 };
 
