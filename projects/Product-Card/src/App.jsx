242f4ec90f04a0696e281d7b7d332dba52c1613b
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-02 17:41:50 -0300

    Agregue filtrado de productos y muestreo de filtro actual

--- projects/Product-Card/src/App.jsx
+++ projects/Product-Card/src/App.jsx
@@ -1,13 +1,17 @@
 import { ProductCard } from "./components/ProductCard.jsx";
 import { Header } from './components/Header.jsx';
 import { productsJson as initialProductsJson } from './mocks/productsJson.json'
+import { useFilter } from "./hooks/useFilter.js";
 // header - products - footer
 
 export function App(){
+   const { filterProducts, filters } = useFilter()
+   const filteredProducts = filterProducts(initialProductsJson)
+   console.log(filters)
    return(
       <>
          <Header />
-         <ProductCard initialCartState={false} productsJson={initialProductsJson} />
+         <ProductCard initialCartState={false} filteredProducts={filteredProducts} />
       </>
    );
 };
