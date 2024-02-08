996192ba3b7b3729b813c1a9a93c5fe5af85bdb4
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-08 11:23:50 -0300

    Uso de contexto cart para agregar y remover producto del cart y funcion checkeo si producto esta en cart o no

--- projects/Product-Card/src/components/ProductCard.jsx
+++ projects/Product-Card/src/components/ProductCard.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 import { CartFilled, CartNotFilled } from './Icons.jsx';
 import './ProductCard.css'
+import { useCart } from '../hooks/useCart.js';
 
-export function ProductCard({ filteredProducts , initialCartState}){
-  const [cartState, setCart] = useState(initialCartState);
+export function ProductCard({ filteredProducts }){
+  const { cart, addToCart, removeFromCart } = useCart()
 
-  const cartNotFilled = <CartNotFilled />
-  const cartFilled = <CartFilled />
-
-  const cart = cartState ? cartFilled : cartNotFilled ;
-  const bttCartClassName = cartState ? 'prCard-cartButton cartFilled' : 'prCard-cartButton cartNotFilled';
+  const checkProductInCart = (product) =>{
+    return cart.some(item => item.id === product.id)
+  }
 
   const handleClick = () =>{
     setCart(!cartState);
+    
   }
   
   return(
@@ -20,33 +20,36 @@ export function ProductCard({ filteredProducts , initialCartState}){
       <section className='productsWrap'>
         <ul>
           {
-            filteredProducts.map(filteredProducts => (
-              <li key={filteredProducts.id} className="prCard">
-                <div className="prCard-imageContainer">
-                  <img className="prCard-image" alt="Repositor Automatico - Inicio" src={filteredProducts.image} />
-                </div>
+            filteredProducts.map(filteredProducts => {
+              const isProductInCart = checkProductInCart(filteredProducts)
+              console.log(isProductInCart, cart)
+              return(
+                <li key={filteredProducts.id} className="prCard">
+                  <div className="prCard-imageContainer">
+                    <img className="prCard-image" alt="Repositor Automatico - Inicio" src={filteredProducts.image} />
+                  </div>
 
-                <div className="prCard-infoContainer">
-                  <h3 className="prCard-prName">
-                    {filteredProducts.name}
-                  </h3>
-                  <h4 className="prCard-prStock">
-                    Cajas disponibles: {filteredProducts.stock}
-                  </h4>
-                </div>
-                <div className="prCard-buttonContainer">
-                  <button className="prCard-orderButton">
-                    Ordenar
-                  </button>
-                  <button className={bttCartClassName} onClick={handleClick}>
-                    <span className='prCard-cartButton-FillIt'>{cart}</span>
-                    <span className='prCard-cartButton-dontFillIt'>
-                      <CartFilled />
-                    </span>
-                  </button>
-                </div>
-              </li>
-            ))
+                  <div className="prCard-infoContainer">
+                    <h3 className="prCard-prName">
+                      {filteredProducts.name}
+                    </h3>
+                    <h4 className="prCard-prStock">
+                      Cajas disponibles: {filteredProducts.stock}
+                    </h4>
+                  </div>
+                  <div className="prCard-buttonContainer">
+                    <button className="prCard-orderButton">
+                      Ordenar
+                    </button>
+                    <button className= "prCard-cartButton" onClick={() => { isProductInCart ? removeFromCart(filteredProducts) : addToCart(filteredProducts)}}>
+                      {
+                        isProductInCart ? <CartFilled /> : <CartNotFilled />
+                      }
+                    </button>
+                  </div>
+                </li>
+              )
+            })
           }
         </ul>
       </section>
