798ae4261d310e7c3ebc6d57ea5ccb7f054dd9d2
Rodrigo Sturm <rodristurm@gmail.com>
2024-02-08 11:14:12 -0300

    Agregue categoria electric y estilos para el filtro

--- projects/Product-Card/src/components/Filters.jsx
+++ projects/Product-Card/src/components/Filters.jsx
@@ -1,5 +1,6 @@
 import { useId } from "react"
 import { useFilter } from "../hooks/useFilter.js"
+import './Filters.css'
 
 export function Filters(){
   const categoryFilterId = useId()
@@ -13,13 +14,14 @@ export function Filters(){
 
   return(
     <section>
-      <div>
+      <div className="container-filters">
         <label className="label-categoryFilter" htmlFor={categoryFilterId}>Categoria: </label>
         <select name="category" id={categoryFilterId} onChange={handleChangeCategory}>
           <option value="all">Todos</option>
           <option value="screw">Tornillos</option>
           <option value="roller-bearing">Rulemanes</option>
           <option value="ceramic">Ceramicos</option>
+          <option value="electric">Electrico</option>
         </select>
       </div>
     </section>
